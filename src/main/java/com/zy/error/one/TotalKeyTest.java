fa46a3ce1550f3bd2a1189280a927e40da2c1b46
zy-yz <1090712762@qq.com>
2020-03-12 15:21:38 +0800

    ConcurrentHashmap中的工具特性

---
+++ src/main/java/com/zy/error/one/TotalKeyTest.java
@@ -0,0 +1,102 @@
+package com.zy.error.one;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
+import org.springframework.util.StopWatch;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.LongAdder;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Controller
+@RequestMapping("/totalkey")
+public class TotalKeyTest {
+
+    //循环次数
+    private static int LOOP_COUNT = 10000000;
+
+    //线层数量
+    private static int THREAD_COUNT = 10;
+
+    //元素数量
+    private static int ITEM_COUNT = 10;
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    private Map<String ,Long> normaluse() throws InterruptedException{
+        ConcurrentHashMap<String,Long> freqs = new ConcurrentHashMap<>(ITEM_COUNT);
+        ForkJoinPool forkJoinPool = new ForkJoinPool(THREAD_COUNT);
+        forkJoinPool.execute(() -> IntStream.rangeClosed(1,LOOP_COUNT).parallel().forEach(i->{
+            //获取一个随机key
+            String key = "item" + ThreadLocalRandom.current().nextInt(ITEM_COUNT);
+            //通过锁的方式锁住 Map，然后做判断、读取现在的累计值、加 1、保存累加后值的逻辑。
+            synchronized (freqs){
+                if(freqs.containsKey(key)){
+                    //key 存在+1
+                    freqs.put(key,freqs.get(key) + 1);
+                }else {
+                    //key不存在就初始化为1
+                    freqs.put(key,1L);
+                }
+            }
+                }));
+        forkJoinPool.shutdown();
+        forkJoinPool.awaitTermination(1, TimeUnit.HOURS);
+        return freqs;
+    }
+
+    //修改过的
+    private Map<String, Long> gooduse() throws InterruptedException {
+        ConcurrentHashMap<String, LongAdder> freqs = new ConcurrentHashMap<>(ITEM_COUNT);
+        ForkJoinPool forkJoinPool = new ForkJoinPool(THREAD_COUNT);
+        forkJoinPool.execute(() -> IntStream.rangeClosed(1, LOOP_COUNT).parallel().forEach(i -> {
+                    String key = "item" + ThreadLocalRandom.current().nextInt(ITEM_COUNT);
+                    //利用computeIfAbsent()方法来实例化LongAdder，然后利用LongAdder来进行线程安全计数
+                    freqs.computeIfAbsent(key, k -> new LongAdder()).increment();
+                }
+        ));
+        forkJoinPool.shutdown();
+        forkJoinPool.awaitTermination(1, TimeUnit.HOURS);
+        //因为我们的Value是LongAdder而不是Long，所以需要做一次转换才能返回
+        return freqs.entrySet().stream()
+                .collect(Collectors.toMap(
+                        e -> e.getKey(),
+                        e -> e.getValue().longValue())
+                );
+    }
+
+
+    @GetMapping("good")
+    public String good() throws InterruptedException {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start("normaluse");
+        Map<String, Long> normaluse = normaluse();
+        stopWatch.stop();
+        //校验元素数量
+        Assert.isTrue(normaluse.size() == ITEM_COUNT, "normaluse size error");
+        //校验累计总数
+        Assert.isTrue(normaluse.entrySet().stream()
+                        .mapToLong(item -> item.getValue()).reduce(0, Long::sum) == LOOP_COUNT
+                , "normaluse count error");
+        stopWatch.start("gooduse");
+        Map<String, Long> gooduse = gooduse();
+        stopWatch.stop();
+        Assert.isTrue(gooduse.size() == ITEM_COUNT, "gooduse size error");
+        Assert.isTrue(gooduse.entrySet().stream()
+                        .mapToLong(item -> item.getValue())
+                        .reduce(0, Long::sum) == LOOP_COUNT
+                , "gooduse count error");
+        log.info(stopWatch.prettyPrint());
+        return "OK";
+    }
+
+}
