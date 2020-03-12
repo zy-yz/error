66e302b99ead562dd1a87e76756d16f2aff277fc
zy-yz <1090712762@qq.com>
2020-03-12 11:46:02 +0800

    Initial commit

---
+++ src/main/java/com/zy/error/one/ThreadLocalTest.java
@@ -0,0 +1,32 @@
+package com.zy.error.one;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class ThreadLocalTest {
+
+    private ThreadLocal<Integer> currentUser = ThreadLocal.withInitial(() -> null);
+
+
+    @GetMapping("wrong")
+    public Map wrong(@RequestParam("userId") Integer userId) {
+        //设置用户信息之前先查询一次ThreadLocal中的用户信息
+        String before  = Thread.currentThread().getName() + ":" + currentUser.get();
+        //设置用户信息到ThreadLocal
+        currentUser.set(userId);
+        //设置用户信息之后再查询一次ThreadLocal中的用户信息
+        String after  = Thread.currentThread().getName() + ":" + currentUser.get();
+        //汇总输出两次查询结果
+        Map result = new HashMap();
+        result.put("before", before);
+        result.put("after", after);
+        return result;
+    }
+
+}
---
+++ src/test/java/com/zy/error/ErrorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zy.error;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
