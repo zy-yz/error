47bff8767aa35a3ef75da95b67072ebf70884d80
zy-yz <1090712762@qq.com>
2020-04-01 21:06:06 +0800

    copyonwrite

--- src/main/java/com/zy/error/one/CopyOnWriteTest.java
+++ src/main/java/com/zy/error/one/CopyOnWriteTest.java
@@ -14,7 +14,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @Controller
-@RequestMapping("copyonwrite")
+@RequestMapping("/copyonwrite")
 public class CopyOnWriteTest {
 
 
