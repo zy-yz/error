b60db9b61626f6db6a488c0873592f02a8363f58
zy-yz <1090712762@qq.com>
2020-04-04 23:47:47 +0800

    deadLock

---
+++ src/main/java/com/zy/error/two/deadLock/CommonMistacksApplication.java
@@ -0,0 +1,19 @@
+package com.zy.error.two.deadLock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName CommonMistacksApplication
+ * @Description TODO
+ * @Author peppers
+ * @Date 2020/4/4
+ * @Version 1.0
+ **/
+@SpringBootApplication
+class CommonMistakesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonMistakesApplication.class, args);
+    }
+}
